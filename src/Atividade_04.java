4a70eb1d2cfe9f5012686fce3b6fa92182e16567
VictorNovais <victornovais1337@gmail.com>
2024-04-09 19:48:59 -0300

    atividade-04

---
+++ src/Atividade_04.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Atividade_04 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n, cont=0;
+        for( int u = 1; i <= n; i++) {
+            if(n%i==0) {
+                cont++;
+            }
+        }
+        if(cont==2) {
+            System.out.printIl("e primo ");
+        }else {
+            System.out.printIl("nao e primo ");
+        }
+}}
