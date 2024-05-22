483d45acf39ebb4b20e5dbeb321bff4c5c201082
VictorNovais <victornovais1337@gmail.com>
2024-05-22 09:28:38 -0300

    Atividade_22

---
+++ src/Atividade_22.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+public class Atividade_22 {
+    public static void main(String[] args) {
+
+
+            Scanner scanner = new Scanner(System.in);
+
+
+            int linhasA = lerDimensao(scanner, "A", "linhas");
+            int colunasA = lerDimensao(scanner, "A", "colunas");
+            int linhasB = lerDimensao(scanner, "B", "linhas");
+            int colunasB = lerDimensao(scanner, "B", "colunas");
+
+
+            if (colunasA != linhasB) {
+                System.out.println("Não é possível multiplicar a matriz A pela matriz B.");
+                return;
+            }
+
+
+            int[][] matrizA = new int[linhasA][colunasA];
+            int[][] matrizB = new int[linhasB][colunasB];
+
+            System.out.println("Digite os valores da matriz A:");
+            lerMatriz(scanner, matrizA);
+
+            System.out.println("Digite os valores da matriz B:");
+            lerMatriz(scanner, matrizB);
+
+
+            int[][] matrizC = multiplicarMatrizes(matrizA, matrizB);
+
+
+            System.out.println("Matriz resultante da multiplicação (A x B):");
+            exibirMatriz(matrizC);
+
+            scanner.close();
+        }
+
+        public static int lerDimensao(Scanner scanner, String matrizNome, String dimensaoNome) {
+            int dimensao;
+            do {
+                System.out.printf("Digite o número de %s da matriz %s (máximo 20):%n", dimensaoNome, matrizNome);
+                dimensao = scanner.nextInt();
+                if (dimensao < 1 || dimensao > 20) {
+                    System.out.println("Dimensão inválida. Digite um valor entre 1 e 20.");
+                }
+            } while (dimensao < 1 || dimensao > 20);
+            return dimensao;
+        }
+
+        public static void lerMatriz(Scanner scanner, int[][] matriz) {
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    System.out.printf("Digite o elemento [%d][%d]: ", i + 1, j + 1);
+                    matriz[i][j] = scanner.nextInt();
+                }
+            }
+        }
+
+        public static int[][] multiplicarMatrizes(int[][] matrizA, int[][] matrizB) {
+            int linhasA = matrizA.length;
+            int colunasA = matrizA[0].length;
+            int colunasB = matrizB[0].length;
+            int[][] matrizC = new int[linhasA][colunasB];
+
+            for (int i = 0; i < linhasA; i++) {
+                for (int j = 0; j < colunasB; j++) {
+                    matrizC[i][j] = 0;
+                    for (int k = 0; k < colunasA; k++) {
+                        matrizC[i][j] += matrizA[i][k] * matrizB[k][j];
+                    }
+                }
+            }
+            return matrizC;
+        }
+
+        public static void exibirMatriz(int[][] matriz) {
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    System.out.print(matriz[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+}
