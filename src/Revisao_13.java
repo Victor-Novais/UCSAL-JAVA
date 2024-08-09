32cb936b12f684bf8db4ee09e8c33eedefaba726
VictorNovais <victornovais1337@gmail.com>
2024-08-08 23:06:19 -0300

    Revisao_13

---
+++ src/Revisao_13.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Revisao_13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+
+        if (ehPrimo(numero)) {
+            System.out.println(numero + " é um número primo.");
+        } else {
+            System.out.println(numero + " não é um número primo.");
+        }
+    }
+
+    public static boolean ehPrimo(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
